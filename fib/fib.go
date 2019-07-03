d5bf79f651bf8cc2052f62a0de83d608c638f02d
N3UC <n3uc@n3uc.com>
2019-07-03 19:17:12 +0000

    Added Fib

---
+++ fib/fib.go
@@ -0,0 +1,16 @@
+package fib
+
+// Return a slice of fibinoacci numbers
+// NOTE: contains a 0 at element 0 so indexing is correct
+func fibSliceMax(max int) []int {
+	fibslice := []int{0, 1, 2}
+	fib := 3
+	idx := 3
+	for fib < max {
+		fib = fibslice[idx-2] + fibslice[idx-1]
+		fibslice = append(fibslice, fib)
+		idx++
+	}
+	return fibslice
+}
+
