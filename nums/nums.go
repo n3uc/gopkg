4c30f5626c89ddf9e2cd97bf172719803a862337
n3uc <n3uc@n3uc.com>
2019-07-10 18:41:36 -0400

    Added nums package

---
+++ nums/nums.go
@@ -0,0 +1,31 @@
+package nums
+
+// SumSlice sums up all elements in an int array
+func SumSlice(list []int) int {
+	sum := 0
+	for _, v := range list {
+		sum += v
+	}
+	return sum
+}
+
+// GCD greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// LCM find Least Common Multiple (LCM) via GCD
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
+}
