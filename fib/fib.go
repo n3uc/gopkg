6687217e22d6a457d9c8c09ceb990d0cebf00bcb
n3uc <n3uc@n3uc.com>
2019-07-19 15:26:20 -0400

    Fixed SliceMax to return 0 1 1 2 3 ... and to not include a number > max in the slice.   Also added SliceMax64 for int64

--- fib/fib.go
+++ fib/fib.go
@@ -1,16 +1,27 @@
 package fib
 
-// Return a slice of fibinoacci numbers
-// NOTE: contains a 0 at element 0 so indexing is correct
+// SliceMax Returns a slice of fibinoacci numbers
 func SliceMax(max int) []int {
-	fibslice := []int{0, 1, 2}
-	fib := 3
-	idx := 3
+	fibslice := []int{0, 1}
+	fib := 1
+	idx := 2
 	for fib < max {
-		fib = fibslice[idx-2] + fibslice[idx-1]
 		fibslice = append(fibslice, fib)
+		fib += fibslice[idx-1]
 		idx++
 	}
 	return fibslice
 }
 
+// SliceMax64 Return a slice of fibinoacci numbers int64 version
+func SliceMax64(max int64) []int64 {
+	fibslice := []int64{0, 1}
+	fib := int64(1)
+	idx := 2
+	for fib < max {
+		fibslice = append(fibslice, fib)
+		fib += fibslice[idx-1]
+		idx++
+	}
+	return fibslice
+}
